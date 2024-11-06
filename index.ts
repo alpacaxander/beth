092e5552c8419aeadf1b5903ff3ba5dd8056655d
Alexander Paulsell <alexander.dean.paulsell@gmail.com>
2024-11-06 13:25:43 -0800

    Update source to send plain html with elysia html and html const

--- index.ts
+++ index.ts
@@ -1,7 +1,26 @@
+import html from "@elysiajs/html";
 import { Elysia } from "elysia";
 
-const app = new Elysia().get("/", () => "Hello World!").listen(3000)
+const app = new Elysia()
+    .use(html())
+    .get("/", ({ html }) => html(baseHtml))
+    .listen(3000)
+
 console.log(
     `Elysia is running on http://${app.server?.hostname}:${app.server?.port}`
 )
+
+const baseHtml = `
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Beth Example</title>
+</head>
+<body>
+    HTML DOC
+</body>
+`
 
