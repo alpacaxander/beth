e1768a95040ba6f9fb631cdf67e547931d9ff248
Alexander Paulsell <alexander.dean.paulsell@gmail.com>
2024-11-08 14:57:58 -0800

    Update base route to get the list on load

--- index.tsx
+++ index.tsx
@@ -2,26 +2,63 @@ import { html } from "@elysiajs/html";
 import { Elysia } from "elysia";
 import * as elements from "typed-html";
 
+type Todo = {
+    id: number,
+    name: string,
+    checked: boolean
+}
+
+const db: Todo[] = [
+    {
+        id: 1,
+        name: "Learn vim motions you slow fuck",
+        checked: false
+    },
+    {
+        id: 2,
+        name: "Fill out your journel for today",
+        checked: true
+    }
+]
+
 const app = new Elysia()
     .use(html())
     .get("/", ({ html }) => html(
         <BaseHtml>
-            <body class="flex w-full h-screen justify-center items-center">
-                <h1>Basic HTML</h1>
-                <button
-                    class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full"
-                    hx-post="/clicked"
-                    hx-swap="outerHTML">
-                    Click Me
-                </button>
+            <body 
+                class="flex w-full h-screen justify-center items-center"
+                hx-get="/list"
+                hx-trigger="load"
+            >
+                spinner
             </body>
         </BaseHtml>
     ))
     .post("/clicked", () => <div> I was rendered on the server </div>)
+    .get("/list", () => <TodoList todos={db}/>)
     .listen(3000)
 
 console.log(
     `Elysia is running on http://${app.server?.hostname}:${app.server?.port}`
+)
+
+const Todo = ({id, name, checked}: Todo) => (
+    <div>
+        <p>{name}</p>
+        <input type="checkbox" checked={checked}></input>
+        <button class="text-red-500">X</button>
+    </div>
+)
+
+const TodoList = ({todos}: {todos: Todo[]}) => (
+    <ul>
+        {
+            todos.map((todo) => (
+                <li><Todo {...todo}/></li>
+            ))
+        }
+        
+    </ul>
 )
 
 const BaseHtml = ({ children }: any /*elements.Children*/ ) => `
